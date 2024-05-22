f855d09fad5713352b19f2e86cd0a1d98ef8cb34
Guilherme de Castro Fernandes <guilherme.castro.fernandes@gmail.com>
2024-05-21 21:03:21 -0300

    feat: add check card

--- main/main.go
+++ main/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
 	"log"
 	"os"
@@ -17,12 +18,20 @@ func main() {
 
 }
 
+func loadEnv() {
+
+	err := godotenv.Load(".env")
+	if err != nil {
+		log.Fatal("Failed to load file .env:", err)
+	}
+}
+
 func readPdf() {
 	// Open PDF file.
 
 	pdfFile, err := os.Open(os.Getenv("PDF_PATH"))
 	if err != nil {
-		fmt.Println("Erro ao abrir o arquivo:", err)
+		fmt.Println("Failed to open file:", err)
 
 	}
 
@@ -30,59 +39,92 @@ func readPdf() {
 	reader, err := model.NewPdfReader(pdfFile)
 
 	if err != nil {
-		fmt.Println("Erro ao criar leitor de PDF:", err)
+		fmt.Println("Failed to create a pdf file:", err)
 
 	}
 
 	// Get number of pages
 	numPages, err := reader.GetNumPages()
 	if err != nil {
-		fmt.Println("Erro ao obter número de páginas:", err)
+		fmt.Println("Failed to get number of pages:", err)
 
 	}
-
-	// Read each page
 
 	for i := 2; i <= numPages; i++ {
 		// Get main Page
 		page, err := reader.GetPage(i)
 		if err != nil {
-			fmt.Println("Erro ao obter página:", err)
+			fmt.Println("Failed to get main page", err)
 			continue
 		}
 
 		//Get text from page.
-		text, err := page.GetContentStreams()
+		lines, err := page.GetContentStreams()
 		if err != nil {
-			fmt.Println("Erro ao extrair texto da página:", err)
+			fmt.Println("Failed to extract text from page:", err)
 			continue
 		}
 
-		// Show text
-		fmt.Println("Página", i, ":")
+		result := filter(lines)
+		fmt.Println(result)
 
-		filterValue(strings.Join(text, ", "))
-
-	}
-
-}
-
-func filterValue(result string) {
-	re := regexp.MustCompile(`\([^)]+\)`)
-	matches := re.FindAllString(result, -1)
-
-	// Show extracted infos
-	for _, match := range matches {
-		fmt.Println(match[1 : len(match)-1]) // Remove parentheses
 	}
 }
 
-func loadEnv() {
+func filter(lines []string) []string {
+	regexData := regexp.MustCompile(`^\d{2}/\d{2}$`) // Format DD/MM
+	arrayCount := 0
 
-	err := godotenv.Load(".env")
-	if err != nil {
-		log.Fatal("Erro ao carregar arquivo .env:", err)
+	catchedLines := []string{}
+	for _, line := range lines {
+		var buffer bytes.Buffer
+
+		re := regexp.MustCompile(`\([^)]+\)`)
+		matches := re.FindAllString(line, -1)
+		for _, match := range matches {
+			extrated := match[1 : len(match)-1] // Remove parentheses due there are parentheses when read a pdf file.
+			fmt.Fprintln(&buffer, extrated)
+
+		}
+		lineWithFilter := buffer.String()
+		lineIntoArray := strings.Split(lineWithFilter, "\n")
+
+		for _, line := range lineIntoArray {
+			array := lineIntoArray
+			arrayCount++
+
+			if haveCard(line) {
+				fmt.Println(line)
+			}
+
+			if regexData.MatchString(line) {
+				position := arrayCount
+
+				store := array[position]
+				value := array[position+1]
+				fmt.Println(store)
+				fmt.Println(value)
+				fmt.Println(line)
+
+			}
+		}
+
 	}
+
+	return catchedLines
+
+}
+
+func haveCard(line string) bool {
+	cards := []string{os.Getenv("FIRST_USER_CARD"), os.Getenv("FIRST_USER_VIRTUAL_CARD"),
+		os.Getenv("SECOND_USER_CARD"), os.Getenv("SECOND_USER_VIRTUAL_CARD")}
+
+	for _, card := range cards {
+		if strings.Contains(line, card) {
+			return true
+		}
+	}
+	return false
 }
 
 //TODO: Find "Detalhamento da Fatura na leitura" and use this as a header.
