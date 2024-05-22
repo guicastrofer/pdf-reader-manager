b1e420146e286a15bff968a9cb2c536d38809eae
Guilherme de Castro Fernandes <guilherme.castro.fernandes@gmail.com>
2024-05-22 08:16:12 -0300

    fix: fixed return from cards

--- main/main.go
+++ main/main.go
@@ -20,6 +20,7 @@ type Card struct {
 }
 
 func main() {
+
 	loadEnv()
 	readPdf()
 
@@ -35,6 +36,10 @@ func loadEnv() {
 
 func readPdf() {
 	// Open PDF file.
+
+	cards := []Card{}
+	card := Card{}
+	name := ""
 
 	pdfFile, err := os.Open(os.Getenv("PDF_PATH"))
 	if err != nil {
@@ -56,7 +61,6 @@ func readPdf() {
 		fmt.Println("Failed to get number of pages:", err)
 
 	}
-
 	for i := 2; i <= numPages; i++ {
 		// Get main Page
 		page, err := reader.GetPage(i)
@@ -72,58 +76,53 @@ func readPdf() {
 			continue
 		}
 
-		filter(lines)
+		regexData := regexp.MustCompile(`^(0[1-9]|[12][0-9]|3[01])/(0[1-9]|1[0-2])$`)
+
+		arrayCount := 0
+
+		for _, line := range lines {
+			var buffer bytes.Buffer
+
+			re := regexp.MustCompile(`\([^)]+\)`)
+			matches := re.FindAllString(line, -1)
+			for _, match := range matches {
+				extrated := match[1 : len(match)-1] // Remove parentheses due there are parentheses when read a pdf file.
+				fmt.Fprintln(&buffer, extrated)
+
+			}
+			lineWithFilter := buffer.String()
+			lineIntoArray := strings.Split(lineWithFilter, "\n")
+
+			for _, line := range lineIntoArray {
+				array := lineIntoArray
+				arrayCount++
+
+				if haveCard(line) {
+					name = line
+
+				}
+
+				if regexData.MatchString(line) {
+
+					position := arrayCount
+
+					store := array[position]
+					value := array[position+1]
+					card.Store = store
+					card.Value = value
+					card.Date = line
+					card.Name = name
+					cards = append(cards, card)
+
+				}
+			}
+
+		}
 
 	}
-}
 
-func filter(lines []string) {
-	cards := []Card{}
-	card := Card{}
-	name := ""
-
-	regexData := regexp.MustCompile(`^\d{2}/\d{2}$`) // Format DD/MM
-	arrayCount := 0
-
-	for _, line := range lines {
-		var buffer bytes.Buffer
-
-		re := regexp.MustCompile(`\([^)]+\)`)
-		matches := re.FindAllString(line, -1)
-		for _, match := range matches {
-			extrated := match[1 : len(match)-1] // Remove parentheses due there are parentheses when read a pdf file.
-			fmt.Fprintln(&buffer, extrated)
-
-		}
-		lineWithFilter := buffer.String()
-		lineIntoArray := strings.Split(lineWithFilter, "\n")
-
-		for _, line := range lineIntoArray {
-			array := lineIntoArray
-			arrayCount++
-
-			if haveCard(line) {
-				name = line
-			}
-
-			if regexData.MatchString(line) {
-				position := arrayCount
-
-				store := array[position]
-				value := array[position+1]
-				card.Store = store
-				card.Value = value
-				card.Date = line
-				card.Name = name
-				cards = append(cards, card)
-
-			}
-		}
-
-		for _, card := range cards {
-			fmt.Printf("Card Details:\n  Date:  %s\n  Store: %s\n  Value: %s\n  Name:  %s\n", card.Date, card.Store, card.Value, card.Name)
-		}
-
+	for _, currentCard := range cards {
+		fmt.Printf("Card Details:\n  Date:  %s\n  Store: %s\n  Value: %s\n  Name:  %s\n", currentCard.Date, currentCard.Store, currentCard.Value, currentCard.Name)
 	}
 
 }
@@ -138,4 +137,17 @@ func haveCard(line string) bool {
 		}
 	}
 	return false
+}
+
+func sortCards(cards []Card) []Card {
+	currentCards := cards
+	countCards := 0
+	for _, card := range cards {
+		countCards++
+		if card.Name == "" {
+			previousCard := currentCards[countCards-1]
+			card.Name = previousCard.Name
+		}
+	}
+	return cards
 }
